0bf50761ae2b2d1ad9813f1b8ff9c8af9668b605
Samkuran <124678532+Samkuran@users.noreply.github.com>
2023-03-02 10:02:22 -0300

    Resolvendo o bug da funçao ap_07
    Para cada função, ter sua variável única

--- eventos.js
+++ eventos.js
@@ -1,9 +1,7 @@
 //ALGORITMOS CONDICIONAIS
 
-let numeros = [];
-
 function ac_03(){
-  
+    let numeros = [];
     numeros.push(ac03.value);
     
     if(numeros % 2 == 0){
@@ -14,13 +12,18 @@ function ac_03(){
 
 //ALGORITMOS REPETIÇÃO
 
-function ap_07(){
-    event.preventDefault();
-    numeros.push(ap07.value);
 
-    for (n = 1; n <= 10; n++) {
-       resposta.innerHTML += `<li>${n}x${numeros}=${n*numeros}</li>`;
-    } 
- 
+function ap_07(){
+    let numeros = [];
+        event.preventDefault();
+        numeros.push(ap07.value);
+        resposta.innerHTML = "";
+        
+        console.log(numeros);
+
+        for (let n = 1; n <= 10; n++) {
+           resposta.innerHTML += `<li>${n}x${numeros}=${n*numeros}</li>`;
+        }     
 }
+
 
